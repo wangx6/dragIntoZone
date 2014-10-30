cfa3decabd893529a7c04149747d57fabab6994d
Xin Wang <xin@xwerx.com>
2014-10-30 10:29:41 +0000

    change animation and eliminate potential jquery lag issue

--- dragMe.js
+++ dragMe.js
@@ -1,34 +1,41 @@
-(function($){
+(function( $ ){
     'use strict';
     
-    var 
+    var
     config = {
         transitionSpeed: 100
     },
-    grid = $('.grid'),
-    blk = $('.blk'),
-    numberOfGrid = grid.length,
+    grid = null,
+    blk = null,
+    numberOfGrid = 0,
     getWidth = function(){ return $('body').width(); },
     getCenterCo = function( obj ){
-        var pos = obj.offset();     
-        return{ x: pos.left + obj.width() / 2, y:pos.top + obj.height() / 2 };
+        var pos = obj.offset();
+        return{ x : pos.left + obj.width() / 2, y : pos.top + obj.height() / 2 };
     };
     
-    grid.css({ width: 1/numberOfGrid * 100 + '%' });
-    blk.css({ width: 1/numberOfGrid * 100 + '%' });
-    
-    blk.mousemove(function( e ){
-        var w = blk.width();
-        if( e.which === 1 || e.buttons === 1 ){
-            $(this).addClass('tilt').css({
-                left:e.pageX - w/2 - 10,
-                top: e.pageY - 40
-            });
-        }
-    }).mouseup(function(){
-        var co = getCenterCo($(this));
-        var v = Math.floor( co.x / getWidth() / ( 1/ numberOfGrid ) ) * 1/numberOfGrid * 100;
-        $(this).removeClass('tilt').animate({ top: 0, left: v+'%' },config.transitionSpeed);
+    $(function () {
+        grid = $('.grid');
+        blk = $('.blk');
+        numberOfGrid = grid.length,
+        
+        grid.css({ width: 1/numberOfGrid * 100 + '%' });
+        blk.css({ width: 1/numberOfGrid * 100 + '%' });
+
+        blk.mousemove(function( e ){
+            var w = blk.width();
+            if( e.which === 1 || e.buttons === 1 ){
+                $(this).addClass('tilt').css({
+                    left : e.pageX - w/2 - 10,
+                    top  : e.pageY - 40
+                });
+            }
+        }).mouseup(function(){
+            var
+            co = getCenterCo($(this)),
+            v = Math.floor( co.x / getWidth() / ( 1/ numberOfGrid ) ) * 1/numberOfGrid * 100;
+            $(this).removeClass('tilt').css({ top: 0, left: v+'%' });
+        });
     });
-    
-})(jQuery);
+
+})( jQuery );
