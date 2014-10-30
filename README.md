3f8f2d2a2ac791ba4076990631a224dc61ed8729
Xin Wang <xin@xwerx.com>
2014-10-30 17:35:00 +0000

    add link to live project

--- README.md
+++ README.md
@@ -2,4 +2,4 @@ Screen Shot:
 ![ScreenShot](https://raw.github.com/wangx6/draggableMeIntoZone/master/img/screenshot.jpg)
 
 Link:
-[<h1><a href="https://cdn.rawgit.com/wangx6/draggableMeIntoZone/master/index.html">Click here to view demo</a></h1>]
+[<h3><a href="https://cdn.rawgit.com/wangx6/draggableMeIntoZone/master/index.html">Click here to view demo</a></h3>]
--- dragMe.js
+++ dragMe.js
@@ -1,95 +1,86 @@
-(function( $ ){
+var Item = function(){
+    this.title = 'crazy';
+    this.content = null;
+    this.location = {
+        group:null,
+        order:null
+    };
+    this.wrap = $('<div>').addClass('item-wrap');
+    this.heading = $('<h3>').addClass('item-heading');
+    this.main = $('<div>').addClass('item-main');
+
+    this.init();
+};
+Item.prototype.init = function(){
+    this.wrap.append(this.heading).append(this.main);
+};
+
+var Grid = function(){
+    this.wrap = $('<div>').addClass('grid');
+    this.cache = [];
+};
+
+var Board = function(){
+    this.data = [
+        {
+            group : 1,
+            info:[
+                {
+                    title : 'title 11',
+                    content : 'content 11'
+                },
+                {
+                    title : 'title 12',
+                    content : 'content 12'
+                }
+            ]
+        },
+        {
+            group : 2,
+            info:[{
+                title : 'title 21',
+                content : 'content 21'
+            }]
+        }
+    ];
+
+    this.wrap = $('.wrap');
+    this.numberOfGrid = null;
+    this.cache = [];
+
+    this.init();
+};
+
+Board.prototype.init = function(){
     'use strict';
     
-    var
-    config = {
-        transitionSpeed: 100
-    },
-    grid = null,
-    blk = null,
-    numberOfGrid = 0,
-    // testInfo = [
-    //     {
-    //         group : 1;
-    //         title : 'title 11',
-    //         content : 'content 11'
-    //     }
-    //     {
-    //         group : 1;
-    //         title : 'title 12',
-    //         content : 'content 12'
-    //     }
-    //     {
-    //         group : 2;
-    //         title : 'title 21',
-    //         content : 'content 21'
-    //     }
-    //     {
-    //         group : 2;
-    //         title : 'title 22',
-    //         content : 'content 22'
-    //     }
-    //     {
-    //         group : 2;
-    //         title : 'title 23',
-    //         content : 'content 23'
-    //     }
-    //     {
-    //         group : 2;
-    //         title : 'title 24',
-    //         content : 'content 24'
-    //     }
-    //     {
-    //         group : 2;
-    //         title : 'title 25',
-    //         content : 'content 25'
-    //     }
-    //     {
-    //         group : 3;
-    //         title : 'title 31',
-    //         content : 'content 31'
-    //     }
-    //     {
-    //         group : 3;
-    //         title : 'title 32',
-    //         content : 'content 32'
-    //     }
-    //     {
-    //         group : 3;
-    //         title : 'title 33',
-    //         content : 'content 33'
-    //     }
-    // ],
-
-
-    getWidth = function(){ return $('body').width(); },
-    getCenterCo = function( obj ){
-        var pos = obj.offset();
-        return{ x : pos.left + obj.width() / 2, y : pos.top + obj.height() / 2 };
-    };
+    var 
+    self = this,
+    grid,
+    item;
     
-    $(function () {
-        grid = $('.grid');
-        blk = $('.blk');
-        numberOfGrid = grid.length,
-        
-        grid.css({ width: 1/numberOfGrid * 100 + '%' });
-        blk.css({ width: 1/numberOfGrid * 100 + '%' });
-
-        blk.mousemove(function( e ){
-            var w = blk.width();
-            if( e.which === 1 || e.buttons === 1 ){
-                blk.addClass('tilt').css({
-                    left : e.pageX - w/2 - 10,
-                    top  : e.pageY - 40
-                });
-            }
-        }).mouseup(function(){
-            var
-            co = getCenterCo( blk ),
-            v = Math.floor( co.x / getWidth() / ( 1/ numberOfGrid ) ) * 1/numberOfGrid * 100;
-            blk.removeClass('tilt').css({ top: 0, left: v+'%' });
+    this.numberOfGrid = this.data.length;
+    this.data.map(function( g ){
+        grid = new Grid();
+        grid.wrap
+        .addClass('grid')
+        .appendTo(self.wrap)
+        .css({
+            width:1/self.numberOfGrid * 100 + '%',
         });
+
+        g.map(function(){
+            item = new Item();
+            item.wrap.appendTo( grid );
+        });
+        this.cache.push(grid);
+       
     });
+};
+
+var board = new Board();
+
+(function( $ ){
+    'use strict';
 
 })( jQuery );
